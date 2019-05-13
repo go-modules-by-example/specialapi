f3c50bb8474ad0bc994640387832ef5af555928f
myitcv <myitcv@example.com>
2019-05-13 15:09:18 +0000

    v1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-modules-by-example/specialapi
+
+go 1.12
---
+++ specialapi.go
@@ -0,0 +1,9 @@
+package specialapi
+
+const Number = 42
+
+const RandomNumber = 4
+
+func Name() string {
+	return "Rob Pike"
+}
